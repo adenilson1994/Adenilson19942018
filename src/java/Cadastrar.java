dc642ca5aae4fee918169e05d68b1acbd45ae806
Magnta <Magnta@DESKTOP-QQ3ASHN>
2018-10-13 09:17:10 -0300

    projeto anderson

---
+++ src/java/Cadastrar.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ *
+ * @author anderson
+ */
+@WebServlet(urlPatterns = {"/Cadastrar"})
+public class Cadastrar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try {
+                       
+            String id = request.getParameter("id");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c =  DriverManager.getConnection("jdbc:mysql://localhost/contabancaria","root","xamp,xamp");
+            PreparedStatement  p =  c.prepareStatement("delete from contacorrente where id = ? ");
+            p.setInt(1,  Integer.parseInt(id));
+            p.execute() ;
+            response.getWriter().print("deletado com sucesso");
+        } catch (SQLException ex) {
+          response.getWriter().print("Erro: " + ex.getMessage());
+          Logger.getLogger(Cadastrar.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Cadastrar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.sendRedirect("index.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+                       
+            String Login = request.getParameter("Login");
+            String Cpf = request.getParameter("Cpf");
+            String RG = request.getParameter("RG");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c =  DriverManager.getConnection("jdbc:mysql://localhost/contabancaria","root","xamp,xamp");
+            PreparedStatement  p =  c.prepareStatement("insert into conta (Login,Cpf,RG) values (?,?,?)");
+            p.setString(1, Login );
+            p.setString(2, Cpf);
+            p.setString(3, RG);
+            p.execute() ;
+            response.sendRedirect("newhtml.html");
+            
+            
+        } catch (SQLException ex) {
+          response.getWriter().print("Erro: " + ex.getMessage());
+          Logger.getLogger(Cadastrar.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Cadastrar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+                
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/cadastro.java
@@ -0,0 +1,8 @@
+
+public class cadastro {
+    String name;{
+    
+    name = null;
+}
+   
+}
